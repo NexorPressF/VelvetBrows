c09481cc29fa569397ae1fbf2d3e6eda34826ad1
NexorPressF <@>
2025-04-19 12:03:12 +0300

    Сортировка по скидке

--- VelvetBrows/VelvetBrows/Views/AdminView.axaml.cs
+++ VelvetBrows/VelvetBrows/Views/AdminView.axaml.cs
@@ -15,13 +15,39 @@ public partial class AdminView : UserControl
     public AdminView()
     {
         InitializeComponent();
+        FCB.SelectedIndex = 0;
         LoadData();
+        
+
     }
 
     private void LoadData()
     {
         Help.TC.Services.Load();
-        ODB.ItemsSource = Help.TC.Services.ToList();
+        if (FCB.SelectedIndex == 0)
+        {
+            ODB.ItemsSource = Help.TC.Services.ToList(); 
+        }
+        if (FCB.SelectedIndex == 1)
+        {
+            ODB.ItemsSource = Help.TC.Services.Where(el => el.Discount >= 0 && el.Discount < 5).ToList();
+        }
+        if (FCB.SelectedIndex == 2)
+        {
+            ODB.ItemsSource = Help.TC.Services.Where(el => el.Discount >= 5 && el.Discount < 15).ToList();
+        }
+        if (FCB.SelectedIndex == 3)
+        {
+            ODB.ItemsSource = Help.TC.Services.Where(el => el.Discount >= 15 && el.Discount < 30).ToList();
+        }
+        if (FCB.SelectedIndex == 4)
+        {
+            ODB.ItemsSource = Help.TC.Services.Where(el => el.Discount >= 30 && el.Discount < 70).ToList();
+        }
+        if (FCB.SelectedIndex == 5)
+        {
+            ODB.ItemsSource = Help.TC.Services.Where(el => el.Discount >= 70 && el.Discount < 100).ToList();
+        }
     }
 
     private void AddBtn_OnClick(object? sender, RoutedEventArgs e)
@@ -57,5 +83,10 @@ public partial class AdminView : UserControl
     private void FindTb_OnTextChanged(object? sender, TextChangedEventArgs e)
     {
         ODB.ItemsSource = Help.TC.Services.Where(el => el.Description.Contains(FindTb.Text) || el.Title.Contains(FindTb.Text)).ToList();
+    }
+
+    private void FCB_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        LoadData();
     }
 }
