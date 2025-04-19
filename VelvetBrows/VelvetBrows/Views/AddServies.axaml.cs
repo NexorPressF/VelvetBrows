ad2b24f18125f7784ff59e44f24c7a0ea20e6247
NexorPressF <@>
2025-04-19 11:35:01 +0300

    Сделал поиск и кнопки для изменения базы данных

--- VelvetBrows/VelvetBrows/Views/AddServies.axaml.cs
+++ VelvetBrows/VelvetBrows/Views/AddServies.axaml.cs
@@ -1,13 +1,43 @@
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using VelvetBrows.Classes;
+using VelvetBrows.Data;
 
 namespace VelvetBrows.Views;
 
 public partial class AddServies : UserControl
 {
-    public AddServies()
+    public int _id;
+    public AddServies(int id = -1)
     {
         InitializeComponent();
+        _id = id;
+        if (id == -1)
+        {
+            MainSp.DataContext = new Service();
+        }
+        else
+        {
+            var service = Help.TC.Services.FirstOrDefault(el => el.Id == id);
+            MainSp.DataContext = service;
+        }
+    }
+
+    private void CancelBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Help.MCC.Content = new AdminView();
+    }
+
+    private void OkBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (_id == -1)
+        {
+            Help.TC.Services.Add(MainSp.DataContext as Service);
+        }
+        Help.TC.SaveChanges();
+        Help.MCC.Content = new AdminView();
     }
 }
--- VelvetBrows/VelvetBrows/Views/AdminView.axaml.cs
+++ VelvetBrows/VelvetBrows/Views/AdminView.axaml.cs
@@ -4,7 +4,9 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Microsoft.EntityFrameworkCore;
+using MsBox.Avalonia;
 using VelvetBrows.Classes;
+using VelvetBrows.Data;
 
 namespace VelvetBrows.Views;
 
@@ -24,11 +26,36 @@ public partial class AdminView : UserControl
 
     private void AddBtn_OnClick(object? sender, RoutedEventArgs e)
     {
-        
+        Help.MCC.Content = new AddServies();
     }
 
     private void RemakeBtn_OnClick(object? sender, RoutedEventArgs e)
     {
-        
+        var ser = ODB.SelectedItem as Service;
+        if (ser == null)
+        {
+            MessageBoxManager.GetMessageBoxStandard("Ошибка", "Не выбран пользователь").ShowAsync();
+            return;
+        }
+
+        Help.MCC.Content = new AddServies(ser.Id);
+    }
+
+    private void RemoveBtn_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var ser = ODB.SelectedItem as Service;
+        if (ser == null)
+        {
+            MessageBoxManager.GetMessageBoxStandard("Ошибка", "Не выбран пользователь").ShowAsync();
+        }
+
+        Help.TC.Services.Remove(ser);
+        Help.TC.SaveChanges();
+        Help.MCC.Content = new AdminView();
+    }
+
+    private void FindTb_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        ODB.ItemsSource = Help.TC.Services.Where(el => el.Description.Contains(FindTb.Text) || el.Title.Contains(FindTb.Text)).ToList();
     }
 }
